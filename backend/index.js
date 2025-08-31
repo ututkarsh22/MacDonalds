3c99b30e9cf9f3daefeb3d23a93518489b7521e5
Utkarsh Trivedi <utkarshtrivedi.lmp@gmail.com>
2025-08-31 20:32:35 +0530

    ...

--- backend/index.js
+++ backend/index.js
@@ -28,7 +28,7 @@ app.use(express.json());
   'http://localhost:3000',
   'http://127.0.0.1:5173',
   'http://127.0.0.1:3000',
-  'https://mac-donalds-adzpkpuwt-utkarsh-trivedis-projects-9de56539.vercel.app/'
+  'https://mac-donalds-dun.vercel.app/'
   ]
 
 
